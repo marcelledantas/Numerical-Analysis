bc29f4acf525b1a56e514623b81aea1392f31eb5
marcelledantas <marcellemoraesdantas@gmail.com>
2022-05-24 12:50:58 -0300

    updating lab9

--- Lab9/main.c
+++ Lab9/main.c
@@ -17,5 +17,20 @@ int main (void){
   printf("\nForça total em Y: %lf\n", *fy);
   printf("\nForça total em X: %lf\n", *fx);
 
+
+  int i;
+  double x0, y0, n, h, t_final, *x_, *y_;
+  x0 = 200.0, y = 0.0, h = 0.1, t_final = 100, n = 50.0;
+
+  x_ = (double *)malloc(n * sizeof(double));
+  y_ = (double *)malloc(n * sizeof(double));
+
+  simula (x0, y0, t_final, n, x_, y_);
+
+  for(i = 0; i < n; i++){
+    printf("\nVetor x[%d] = %lf \n", i, x_[i]);
+    printf("\nVetor y[%d] = %lf \n", i, y_[i]);
+}
+  
   return 0;
 }
--- Lab9/massamola.c
+++ Lab9/massamola.c
@@ -8,14 +8,14 @@
 void forca (double t, double x, double y, double* fx, double* fy){
   
   double forcaTotalX, forcaTotalY;
-  double k, gx, gy, m, r, wx, wy;
+  double k, gx, gy, m, r, wx, wy, xNorma;
 
   k = 0.3, m = 2.0, gx = 0.0, gy = 9.8, r = 200, wx = 10.0, wy = 0.0;
-
+  xNorma = sqrt(x*x + y*y);
   
-  forcaTotalX = (m * gx) + (wx * exp(-t/20)) + (-k * (fabs(x) - r) * x/fabs(x));
+  forcaTotalX = (m * gx) + (wx * exp(-t/20)) + (-k * (xNorma - r) * x/xNorma);
 
-  forcaTotalY = (m * gy) + (wy * exp(-t/20)) + (-k * (fabs(y) - r) * y/fabs(y));;
+  forcaTotalY = (m * gy) + (wy * exp(-t/20)) + (-k * (xNorma - r) * y/xNorma);
 
   *fy = forcaTotalY;
   *fx = forcaTotalX;
@@ -24,11 +24,11 @@ void forca (double t, double x, double y, double* fx, double* fy){
 
 double evolui (double t, double h, double x, double y, double xp, double yp, double* xn, double* yn ){
 
-  double delta, *fx, *fy;
-  delta = 0.002;
+  double delta, *fx, *fy, m;
+  delta = 0.002, m = 2.0;
 
-  double *fx = (double *)malloc(sizeof(double));
-  double *fy = (double *)malloc(sizeof(double));
+  fx = (double *)malloc(sizeof(double));
+  fy = (double *)malloc(sizeof(double));
 
   forca(t, x, y, fx, fy);
 
@@ -40,5 +40,42 @@ double evolui (double t, double h, double x, double y, double xp, double yp, dou
 
 void simula (double x0, double y0, double t_final, int n, double* x, double* y){
 
+  double h;
+  double tempoCorrente, iter, tH, Xatual = x0, Xantes = x0, Yatual = y0, Yantes = y0;
+  int i = 1;
+  double *ex, *ey;
 
+  iter = 1.0;
+
+  ex = (double *)malloc(sizeof(double));
+  ey = (double *)malloc(sizeof(double));
+
+  x[0] = x0;
+  y[0] = y0;
+
+  while(iter <= n){
+    
+
+    h = t_final/n;
+    tempoCorrente = (iter * h);
+    
+    
+    tH = evolui(tempoCorrente, h, Xatual, Yatual, Xantes, Yantes, ex, ey);
+    
+    //Estado t + h
+    x[i] = *ex;
+    y[i] = *ey;
+
+    //Estado atual
+    Xatual = x[i];
+    Yatual = y[i];
+
+    //Estado anterior
+    Xantes = x[i - 1];
+    Yantes = y[i - 1];
+    
+    i++;
+    iter++;
+  }
+  
 }
